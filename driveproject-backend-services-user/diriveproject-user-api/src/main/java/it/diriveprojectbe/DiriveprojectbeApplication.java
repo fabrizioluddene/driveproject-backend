b4a77000aed359455b87a83c9d8a6ab92d903923
FLUDDENE <fabrizio.luddene@capgemini.com>
2020-08-01 13:33:51 +0200

    first commit

---
+++ driveproject-backend-services-user/diriveproject-user-api/src/main/java/it/diriveprojectbe/DiriveprojectbeApplication.java
@@ -0,0 +1,13 @@
+package it.diriveprojectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiriveprojectbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiriveprojectbeApplication.class, args);
+	}
+
+}
---
+++ driveproject-backend-services-user/diriveproject-user-api/src/test/java/it/diriveprojectbe/DiriveprojectbeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.diriveprojectbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiriveprojectbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
